5d69a9c8e65b6f6b587971828877ad0272e16690
Glitch (handling-events-lesson13-coderx) <none>
2020-05-17 17:04:22 +0000

    ✌️👯 Updated with Glitch

---
+++ src/components/Modal.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+var classNames = require("classnames");
+
+class Modal extends Component {
+  render() {
+    const active = this.props.active;
+    const onExitClick = this.props.onExitClick;
+    return (
+      <div className={classNames('Modal', {active: active})}>
+        <div className="container">
+          <h1>This is a modal 1</h1>
+          <div className="exit" onClick={onExitClick}>❌</div>
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </div>
+          <div className="button">
+            <button className="accept" onClick={onExitClick}>Accept</button>
+            <button className="decline" onClick={onExitClick}>Decline</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
